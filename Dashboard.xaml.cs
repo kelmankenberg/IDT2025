b23a651a10ac8ee0be1b75656eda4bb61661bf78
kel <kellymank@gmail.com>
2024-10-18 15:24:31 -0400

    Cards in place in dashboard

--- Dashboard.xaml.cs
+++ Dashboard.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using IDT2025.Properties;
+using System.Diagnostics;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace IDT2025
@@ -6,11 +8,18 @@ namespace IDT2025
     public partial class Dashboard : UserControl
     {
         public string FirstName => Settings.Default.FirstName;
+        public string CurrentDate => DateTime.Now.ToString("MMMM dd, yyyy");
 
         public Dashboard()
         {
             InitializeComponent();
             DataContext = this;
+        }
+
+        private void Dashboard_Loaded(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine($"Dashboard ActualWidth: {this.ActualWidth}, ActualHeight: {this.ActualHeight}");
+            Debug.WriteLine($"DashboardMainContainer ActualWidth: {DashboardMainContainer.ActualWidth}, ActualHeight: {DashboardMainContainer.ActualHeight}");
         }
     }
 }
